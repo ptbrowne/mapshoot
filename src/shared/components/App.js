9f1bce72bcd5999cc1ebfd80dc69524a3b44d52c
Patrick Browne <pt.browne@gmail.com>
2016-10-23 22:16:09 +0200

    forget cameraType/camera on click

--- src/shared/components/App.js
+++ src/shared/components/App.js
@@ -194,16 +194,20 @@ class App extends React.Component {
   handleClickMap (latlng) {
     var cameraType = this.state.selectedCameraType;
     if (!cameraType) {
-      window.alert('select camera type first');
+      this.setState({ selectedCamera: null });
+    } else {
+      this.state.cameras.push(new Camera({
+        widthInMillimeters: cameraType.widthInMillimeters,
+        heightInMillimeters: cameraType.heightInMillimeters,
+        latlng: [latlng.lat, latlng.lng],
+        zoom: cameraType.defaultZoom,
+        map: this.refs.map.map
+      }));
+      this.setState({
+        cameras: this.state.cameras,
+        selectedCameraType: null
+      });
     }
-    this.state.cameras.push(new Camera({
-      widthInMillimeters: cameraType.widthInMillimeters,
-      heightInMillimeters: cameraType.heightInMillimeters,
-      latlng: [latlng.lat, latlng.lng],
-      zoom: cameraType.defaultZoom,
-      map: this.refs.map.map
-    }));
-    this.setState({ cameras: this.state.cameras });
     this.saveStateLS();
   }
 
