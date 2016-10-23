4ba45cef7008370ed9d0c86e69e748ff77cdf396
Patrick Browne <pt.browne@gmail.com>
2016-10-23 19:00:42 +0200

    fix routes

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,9 @@
 var React       = require('react');
 var { render }  = require('react-dom');
-var { Router, browserHistory }  = require('react-router');
+var { Router, hashHistory }  = require('react-router');
 var routes      = require('../shared/routes');
 
 render(
-  <Router children={routes} history={browserHistory} />,
+  <Router children={routes} history={hashHistory} />,
   document.getElementById('react-view')
 );
