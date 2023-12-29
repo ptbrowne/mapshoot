b98a5e711fc396b427b06528ad2102c63330284b
Patrick Browne <pt.browne@gmail.com>
2023-12-29 18:13:24 +0100

    chore: Add prettier

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import path from 'path'
+import path from "path"
 
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
 // import { visualizer } from 'rollup-plugin-visualizer'
-import { defineConfig } from 'vite'
-import svgrPlugin from 'vite-plugin-svgr'
+import { defineConfig } from "vite"
+import svgrPlugin from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     sourcemap: true
   },
   server: {
     port: 3000
   },
   esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+    logOverride: { "this-is-undefined-in-esm": "silent" }
   },
   plugins: [
     react(),
